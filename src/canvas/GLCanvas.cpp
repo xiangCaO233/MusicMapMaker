7dbaf04ae2361b5084366465d84f9004c41be827
xiangCaO233 <odokoei@outlook.com>
2025-03-28 18:14:07 +0800

    添加gl函数实现

--- src/canvas/GLCanvas.cpp
+++ src/canvas/GLCanvas.cpp
@@ -3,3 +3,7 @@
 GLCanvas::GLCanvas(QWidget *parent) : QOpenGLWidget(parent) {}
 
 GLCanvas::~GLCanvas() = default;
+
+void GLCanvas::initializeGL() {}
+void GLCanvas::resizeGL(int w, int h) {}
+void GLCanvas::paintGL() {}
