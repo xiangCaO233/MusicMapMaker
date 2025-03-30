f1c497a6a017206c78fd42aec415a020731d2e16
xiangCaO233 <odokoei@outlook.com>
2025-03-30 19:09:23 +0800

    添加渲染命令类

---
+++ src/canvas/RenderCommand.cpp
@@ -0,0 +1,5 @@
+#include "RenderCommand.h"
+
+RenderCommand::RenderCommand() {}
+
+RenderCommand::~RenderCommand() {}
---
+++ src/canvas/RenderCommand.h
@@ -0,0 +1,12 @@
+#ifndef RENDERCOMMAND_H
+#define RENDERCOMMAND_H
+
+class RenderCommand {
+ public:
+  // 构造RenderCommand
+  RenderCommand();
+  // 析构RenderCommand
+  virtual ~RenderCommand();
+};
+
+#endif  // RENDERCOMMAND_H
