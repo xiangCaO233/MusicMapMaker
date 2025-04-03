70f894ea04ab5e537130a65e94b436b9f4ccab51
xiangCaO233 <odokoei@outlook.com>
2025-04-03 18:19:43 +0800

    添加绘制测试

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,6 +5,11 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  // 获得画布引用
+  auto &canvas = ui->canvas;
+  auto rect = QRectF(100, 100, 50, 50);
+  canvas->renderer_manager->addRect(rect, nullptr, Qt::red, false);
+  canvas->renderer_manager->renderAll();
 }
 
 MainWindow::~MainWindow() { delete ui; }
